1e68f2cad90398af7b8809755cc1826277352869
TheMaximeSan <mxm.tournier@gmail.com>
2024-09-17 12:28:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,34 +3,33 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
+$status = "true";
+$why = "No request";
 $reponse = "Bienvenue sur l'API de conversion d'image en Base64";
 
 
+if (!empty($_GET['pictureName'])){
 
-if (!empty($_POST['pictureName'])){
+    $reponse = $_GET['pictureName'];
+
+    if (file_exists($_GET['pictureName'])) {
+        $image = file_get_contents($_GET['pictureName']);
 
 
+        $status = "true";
+        $why = "Image good";
+        $reponse = base64_encode($image);
 
-    $reponse = $_POST['pictureName'];
-
+    } else {
+        $status = "err";
+        $why = "Image not found";
+        $reponse = "";
+    }
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-echo json_encode($reponse);
+echo json_encode([
+    "status"=> $status,
+    "why"=> $why,
+    "data" => $reponse
+]);
