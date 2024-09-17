341e5d19d4c2c9de58cca55b1d61605fcaa7b40b
TheMaximeSan <mxm.tournier@gmail.com>
2024-09-17 12:20:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,36 @@
 <?php
 
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+$reponse = "Bienvenue sur l'API de conversion d'image en Base64";
+
+
+
+if (!empty($_POST['pictureName'])){
+
+
+
+    $reponse = $_POST['pictureName'];
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo json_encode($reponse);
